c1de9182f8ac0e7ae211a8d646f30c0f57791183
Haofeng Wang - HomePC <whf11123768@163.com>
2019-05-03 15:50:50 +0800

    [singleton] update test case for serial singleton

--- src/test/java/com/gerrard/design_pattern/u01_singleton/serialize/SerializableSingletonTest.java
+++ src/test/java/com/gerrard/design_pattern/u01_singleton/serialize/SerializableSingletonTest.java
@@ -9,16 +9,25 @@ import java.io.*;
 
 public final class SerializableSingletonTest {
 
-    private static final String uri1 = "./src/test/resources/serialable1.txt";
-    private static final String uri2 = "./src/test/resources/serialable2.txt";
+    private static final String ROOT_DIRECTORY = "./src/test/resources/";
+    private static final String URI1 = ROOT_DIRECTORY + "serial1.txt";
+    private static final String URI2 = ROOT_DIRECTORY + "serial2.txt";
+
+    private static boolean hasRootDirectory = false;
 
     @BeforeAll
     public static void createSerializedFile() {
+        File root = new File(ROOT_DIRECTORY);
+        if (root.exists()) {
+            hasRootDirectory = true;
+        } else {
+            root.mkdir();
+        }
         SerializableSingletonFail singleton1 = SerializableSingletonFail.getInstance();
         SerializableSingleton singleton2 = SerializableSingleton.getInstance();
-        try (FileOutputStream fos1 = new FileOutputStream(uri1);
+        try (FileOutputStream fos1 = new FileOutputStream(URI1);
              ObjectOutputStream oos1 = new ObjectOutputStream(fos1);
-             FileOutputStream fos2 = new FileOutputStream(uri2);
+             FileOutputStream fos2 = new FileOutputStream(URI2);
              ObjectOutputStream oos2 = new ObjectOutputStream(fos2)) {
             oos1.writeObject(singleton1);
             oos2.writeObject(singleton2);
@@ -29,17 +38,20 @@ public final class SerializableSingletonTest {
 
     @AfterAll
     public static void destroySerializedFile() {
-        File file1 = new File(uri1);
-        File file2 = new File(uri2);
+        File file1 = new File(URI1);
+        File file2 = new File(URI2);
         file1.delete();
         file2.delete();
+        if (!hasRootDirectory) {
+            new File(ROOT_DIRECTORY).delete();
+        }
     }
 
     @Test
     void testDeserializeSingletonFailure() {
         SerializableSingletonFail singleton1 = SerializableSingletonFail.getInstance();
         SerializableSingletonFail singleton2 = null;
-        try (FileInputStream fio = new FileInputStream(uri1);
+        try (FileInputStream fio = new FileInputStream(URI1);
              ObjectInputStream ois = new ObjectInputStream(fio)) {
             singleton2 = (SerializableSingletonFail) ois.readObject();
         } catch (Exception e) {
@@ -52,7 +64,7 @@ public final class SerializableSingletonTest {
     void testDeserializeSingletonSuccess() {
         SerializableSingleton singleton1 = SerializableSingleton.getInstance();
         SerializableSingleton singleton2 = null;
-        try (FileInputStream fio = new FileInputStream(uri2);
+        try (FileInputStream fio = new FileInputStream(URI2);
              ObjectInputStream ois = new ObjectInputStream(fio)) {
             singleton2 = (SerializableSingleton) ois.readObject();
         } catch (Exception e) {
@@ -60,5 +72,4 @@ public final class SerializableSingletonTest {
         }
         Assertions.assertSame(singleton1, singleton2);
     }
-
 }
